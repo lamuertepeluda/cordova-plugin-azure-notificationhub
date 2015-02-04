8262a2bb8a76cd7bfb70f993a912a5e42941c69e
Vito Macchia <vito.macchia@gmail.com>
2015-02-04 11:51:46 +0100

    Generic activity name thanks to Francesco Ruà

--- src/android/NotificationHub.java
+++ src/android/NotificationHub.java
@@ -1,6 +1,8 @@
 package msopentech.azure;
 
 import java.util.Set;
+import java.util.List;
+import java.util.Collections;
 
 import android.support.v4.app.NotificationCompat;
 
@@ -18,169 +20,184 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.app.PendingIntent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
-
 
 /**
  * Apache Cordova plugin for Windows Azure Notification Hub
  */
 public class NotificationHub extends CordovaPlugin {
 
-public static final String LOG_TAG = "plugin_log";
-    /**
-     * The callback context from which we were invoked.
-     */
-    protected static CallbackContext _callbackContext = null;
+ public static final String LOG_TAG = "plugin_log";
+ /**
+  * The callback context from which we were invoked.
+  */
+ protected static CallbackContext _callbackContext = null;
 
+ @Override
+ public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+  _callbackContext = callbackContext;
+  try {
+
+   if (action.equals("registerApplication")) {
+    String hubName = args.getString(0);
+    String connectionString = args.getString(1);
+    String senderId = args.getString(4);
+    registerApplication(hubName, connectionString, senderId);
+    return true;
+   }
+
+   if (action.equals("unregisterApplication")) {
+    String hubName = args.getString(0);
+    String connectionString = args.getString(1);
+    unregisterApplication(hubName, connectionString);
+    return true;
+   }
+
+   return false; // invalid action
+  } catch (Exception e) {
+   _callbackContext.error(e.getMessage());
+  }
+  return true;
+ }
+
+ /**
+  * Asynchronously registers the device for native notifications.
+  */
+ @SuppressWarnings("unchecked")
+ private void registerApplication(final String hubName, final String connectionString, final String senderId) {
+
+  try {
+   final GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(cordova.getActivity());
+
+   new AsyncTask() {
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        _callbackContext = callbackContext;
-        try {
+    protected Object doInBackground(Object... params) {
+     try {
+      String gcmId = gcm.register(senderId);
+      JSONObject registrationResult = new JSONObject();
+      registrationResult.put("registrationId", gcmId);
+      registrationResult.put("event", "registerApplication");
 
-            if (action.equals("registerApplication")) {
-                    String hubName = args.getString(0);
-                    String connectionString = args.getString(1);
-                    String senderId = args.getString(4);
-                    registerApplication(hubName, connectionString, senderId);
-                    return true;
-            }
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, registrationResult);
+      // keepKallback is used to continue using the same callback to notify about push notifications received
+      pluginResult.setKeepCallback(true);
 
-            if (action.equals("unregisterApplication")) {
-                String hubName = args.getString(0);
-                String connectionString = args.getString(1);
-                unregisterApplication(hubName, connectionString);
-                return true;
-            }
+      NotificationHub.getCallbackContext().sendPluginResult(pluginResult);
 
-            return false; // invalid action
-        } catch (Exception e) {
-            _callbackContext.error(e.getMessage());
-        }
-        return true;
+     } catch (Exception e) {
+      NotificationHub.getCallbackContext().error(e.getMessage());
+     }
+     return null;
     }
+   }.execute(null, null, null);
+  } catch (Exception e) {
+   NotificationHub.getCallbackContext().error(e.getMessage());
+  }
+ }
 
-    /**
-     * Asynchronously registers the device for native notifications.
-     */
-    @SuppressWarnings("unchecked")
-    private void registerApplication(final String hubName, final String connectionString, final String senderId) {
+ /**
+  * Unregisters the device for native notifications.
+  */
+ private void unregisterApplication(final String hubName, final String connectionString) {
+  try {
+   NotificationHub.getCallbackContext().success();
+  } catch (Exception e) {
+   NotificationHub.getCallbackContext().error(e.getMessage());
+  }
+ }
 
-        try {
-            final GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(cordova.getActivity());
-           
-		   new AsyncTask() {
-                @Override
-                protected Object doInBackground(Object... params) {
-                   try {
-                      String gcmId = gcm.register(senderId);                      
-					  JSONObject registrationResult = new JSONObject();					  
-					  registrationResult.put("registrationId", gcmId);
-                      registrationResult.put("event", "registerApplication");
-                      
-                      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, registrationResult);
-                      // keepKallback is used to continue using the same callback to notify about push notifications received
-                      pluginResult.setKeepCallback(true);
+ /**
+  * Handles push notifications received.
+  */
+ public static class PushNotificationReceiver extends android.content.BroadcastReceiver {
 
-                      NotificationHub.getCallbackContext().sendPluginResult(pluginResult);
+  int NOTIFICATION_ID = 1;
+  private NotificationManager mNotificationManager;
 
-                   } catch (Exception e) {
-                       NotificationHub.getCallbackContext().error(e.getMessage());
-                   }
-                   return null;
-               }
-             }.execute(null, null, null);
-        } catch (Exception e) {
-            NotificationHub.getCallbackContext().error(e.getMessage());
-        }
+  private void sendNotification(Context context, String msg) {
+
+   mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+   String packageName = context.getApplicationContext().getPackageName();
+   Intent intent = new Intent();
+
+   final PackageManager pm = context.getPackageManager();
+
+   Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+   mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+   List<ResolveInfo> appList = pm.queryIntentActivities(mainIntent, 0);
+   Collections.sort(appList, new ResolveInfo.DisplayNameComparator(pm));
+
+   for (ResolveInfo temp : appList) {
+
+    if (temp.activityInfo.packageName.equals(packageName)) {
+     intent.setClassName(context, temp.activityInfo.name);
+//				System.out.println("### package and activity name = "+ temp.activityInfo.name);
+     break;
     }
+   }
 
-    /**
-     * Unregisters the device for native notifications.
-     */
-    private void unregisterApplication(final String hubName, final String connectionString) {
-        try { 
-            NotificationHub.getCallbackContext().success();
-        } catch (Exception e) {
-            NotificationHub.getCallbackContext().error(e.getMessage());
-        }
+   PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+   Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+   String uri = "@drawable/icon";
+   int imageResource = context.getResources().getIdentifier(uri, null, context.getPackageName());
+   String appName = context.getApplicationInfo().loadLabel(context.getPackageManager()).toString();
+
+   NotificationCompat.Builder mBuilder
+           = new NotificationCompat.Builder(context)
+           .setSmallIcon(imageResource) // todo get app icon
+           .setContentTitle(appName)
+           //                .setStyle(new NotificationCompat.BigTextStyle()
+           //                           .bigText())
+           .setContentText(msg)
+           .setSound(alarmSound);
+
+   mBuilder.setAutoCancel(true);
+   mBuilder.setContentIntent(contentIntent);
+
+   mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+
+  }
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+
+   JSONObject json = new JSONObject();
+   String msg = new String();
+   try {
+
+    Set<String> keys = intent.getExtras().keySet();
+    for (String key : keys) {
+     json.put(key, intent.getExtras().get(key));
     }
+    msg = json.getString("message");
 
-    /**
-     * Handles push notifications received.
-     */
-    public static class PushNotificationReceiver extends android.content.BroadcastReceiver {
-        
-        int NOTIFICATION_ID = 1;
-        private  NotificationManager mNotificationManager;
-        
-        private void sendNotification(Context context, String msg){
+   } catch (JSONException e) {
+    e.printStackTrace();
+   }
 
-                 mNotificationManager = (NotificationManager)
-                                 context.getSystemService(Context.NOTIFICATION_SERVICE);
+   if (NotificationHub.getCallbackContext() == null) {
+    sendNotification(context, msg);
+    return;
+   } else {
+    PluginResult result = new PluginResult(PluginResult.Status.OK, json);
+    result.setKeepCallback(true);
+    NotificationHub.getCallbackContext().sendPluginResult(result);
 
-		 PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, it.ismb.Floodis.FloodisApp.class), 0);
+   }
+  }
 
-                 Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                 
-                 String uri = "@drawable/icon";
-                 int imageResource = context.getResources().getIdentifier(uri, null, context.getPackageName());
-                 String appName =context.getApplicationInfo().loadLabel(context.getPackageManager()).toString();
-                 
-                 NotificationCompat.Builder mBuilder =
-                  new NotificationCompat.Builder(context)
-                  .setSmallIcon(imageResource) // todo get app icon
-                  .setContentTitle(appName)
-//                .setStyle(new NotificationCompat.BigTextStyle()
-//                           .bigText())
-                  .setContentText(msg)
-                  .setSound(alarmSound);
+ }
 
-		  mBuilder.setAutoCancel(true);
-                  mBuilder.setContentIntent(contentIntent);
-
-             mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
-                
-        }
-        
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            
-            
-            JSONObject json = new JSONObject();
-            String msg = new String(); 
-            try {
-
-                Set<String> keys = intent.getExtras().keySet();
-                for (String key : keys) {
-                    json.put(key, intent.getExtras().get(key));
-                }
-                msg = json.getString("message");
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }       
-
-                
-
-
-            if (NotificationHub.getCallbackContext() == null){
-                sendNotification(context, msg);
-                return;
-            }else{
-                PluginResult result = new PluginResult(PluginResult.Status.OK, json);
-                result.setKeepCallback(true);
-                NotificationHub.getCallbackContext().sendPluginResult(result);
-            
-            }
-        }
-
-    }
-
-    /**
-     * Returns plugin callback.
-     */
-    protected static CallbackContext getCallbackContext() {
-        return _callbackContext;
-    }
+ /**
+  * Returns plugin callback.
+  */
+ protected static CallbackContext getCallbackContext() {
+  return _callbackContext;
+ }
 }
