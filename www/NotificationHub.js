166799e931d9e21e94cc7ebde7702e43a5bba656
Vito Macchia <vito.macchia@gmail.com>
2015-03-20 18:59:09 +0100

    Fixed module already defined bug

--- www/NotificationHub.js
+++ www/NotificationHub.js
@@ -1,4 +1,5 @@
-cordova.define("msopentech.azure.NotificationHub.NotificationHub", function (require, exports, module) { /*
+//cordova.define("msopentech.azure.NotificationHub.NotificationHub", function (require, exports, module) {
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -19,84 +20,84 @@ cordova.define("msopentech.azure.NotificationHub.NotificationHub", function (req
  *
  */
 
- var exec = require('cordova/exec');
- var Promise = require('./Promise');
+var exec = require('cordova/exec');
+var Promise = require('./Promise');
 
- /**
-  * Initializes a new instance of the NotificationHub class.
-  * http://msdn.microsoft.com/en-us/library/microsoft.windowsazure.messaging.notificationhub.notificationhub.aspx
-  *
-  * @param {string} params Platform specific additional parameters (optional).
-  */
- var NotificationHub = function (params) {
-  //if (typeof notificationHubPath == 'undefined') {
-  //    throw new Error('Please specify notificationHubPath');
-  //}
+/**
+ * Initializes a new instance of the NotificationHub class.
+ * http://msdn.microsoft.com/en-us/library/microsoft.windowsazure.messaging.notificationhub.notificationhub.aspx
+ *
+ * @param {string} params Platform specific additional parameters (optional).
+ */
+var NotificationHub = function (params) {
+ //if (typeof notificationHubPath == 'undefined') {
+ //    throw new Error('Please specify notificationHubPath');
+ //}
 
-  //if (typeof connectionString == 'undefined') {
-  //   throw new Error('Please specify connectionString');
-  //}
-  //this.notificationHubPath = notificationHubPath;
-  //this.connectionString = connectionString;
-  this.platformSpecificParams = params || null;
+ //if (typeof connectionString == 'undefined') {
+ //   throw new Error('Please specify connectionString');
+ //}
+ //this.notificationHubPath = notificationHubPath;
+ //this.connectionString = connectionString;
+ this.platformSpecificParams = params || null;
 
-  this.onPushNotificationReceived = null;
- };
+ this.onPushNotificationReceived = null;
+};
 
- /**
-  * Asynchronously registers the device for native notifications.
-  * http://msdn.microsoft.com/en-us/library/dn339332.aspx
-  *
-  * @param {Array} tags The tags (not supported currently).
-  */
- NotificationHub.prototype.registerApplicationAsync = function (tags) {
-  var globalNotificationHandlerName = 'NotificationHub_onNotificationReceivedGlobal';
-  // global handler that will be called every time new notification is received
-  window[globalNotificationHandlerName] = function (msg) {
-   // if handler attached
-   if (this.onPushNotificationReceived !== null) {
-    this.onPushNotificationReceived(msg);
-   }
-  }.bind(this);
+/**
+ * Asynchronously registers the device for native notifications.
+ * http://msdn.microsoft.com/en-us/library/dn339332.aspx
+ *
+ * @param {Array} tags The tags (not supported currently).
+ */
+NotificationHub.prototype.registerApplicationAsync = function (tags) {
+ var globalNotificationHandlerName = 'NotificationHub_onNotificationReceivedGlobal';
+ // global handler that will be called every time new notification is received
+ window[globalNotificationHandlerName] = function (msg) {
+  // if handler attached
+  if (this.onPushNotificationReceived !== null) {
+   this.onPushNotificationReceived(msg);
+  }
+ }.bind(this);
 
-  var deferral = new Promise.Deferral(),
-    successCallback = function (result) {
-     // registration completeness callback
-     if (result && result.event === 'registerApplication') {
-      delete result.event; // not required
-      deferral.resolve(result);
-     } else { //push notification
-      window[globalNotificationHandlerName](result);
-     }
-    },
-    errorCallback = function (err) {
-     deferral.reject(err);
-    };
-
-  exec(successCallback, errorCallback, 'NotificationHub', 'registerApplication', [globalNotificationHandlerName, tags, this.platformSpecificParams]);
-
-  return deferral.promise;
- };
-
- /**
-  * Asynchronously unregisters the native registration on the application or secondary tiles.
-  * http://msdn.microsoft.com/en-us/library/microsoft.windowsazure.messaging.notificationhub.unregisternativeasync.aspx
-  */
- NotificationHub.prototype.unregisterApplicationAsync = function ()
- {
-  var deferral = new Promise.Deferral(),
-    successCallback = function (result) {
+ var deferral = new Promise.Deferral(),
+   successCallback = function (result) {
+    // registration completeness callback
+    if (result && result.event === 'registerApplication') {
+     delete result.event; // not required
      deferral.resolve(result);
-    },
-    errorCallback = function (err) {
-     deferral.reject(err);
-    };
+    } else { //push notification
+     window[globalNotificationHandlerName](result);
+    }
+   },
+   errorCallback = function (err) {
+    deferral.reject(err);
+   };
 
-  exec(successCallback, errorCallback, 'NotificationHub', 'unregisterApplication', []);
+ exec(successCallback, errorCallback, 'NotificationHub', 'registerApplication', [globalNotificationHandlerName, tags, this.platformSpecificParams]);
 
-  return deferral.promise;
- };
+ return deferral.promise;
+};
 
- module.exports = NotificationHub;
+/**
+ * Asynchronously unregisters the native registration on the application or secondary tiles.
+ * http://msdn.microsoft.com/en-us/library/microsoft.windowsazure.messaging.notificationhub.unregisternativeasync.aspx
+ */
+NotificationHub.prototype.unregisterApplicationAsync = function ()
+{
+ var deferral = new Promise.Deferral(),
+   successCallback = function (result) {
+    deferral.resolve(result);
+   },
+   errorCallback = function (err) {
+    deferral.reject(err);
+   };
 
-});
+ exec(successCallback, errorCallback, 'NotificationHub', 'unregisterApplication', []);
+
+ return deferral.promise;
+};
+
+module.exports = NotificationHub;
+
+//});
