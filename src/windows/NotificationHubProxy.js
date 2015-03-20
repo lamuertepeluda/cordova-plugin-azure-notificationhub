f7d85a243cc74fd967a1a3e7223fe08b43874bb5
Vito Macchia <vito.macchia@gmail.com>
2015-03-20 19:18:34 +0100

    Fixed module already defined bug in windows

--- src/windows/NotificationHubProxy.js
+++ src/windows/NotificationHubProxy.js
@@ -1,4 +1,4 @@
-cordova.define("msopentech.azure.NotificationHub.NotificationHubProxy", function (require, exports, module) { /*
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,75 +18,74 @@ cordova.define("msopentech.azure.NotificationHub.NotificationHubProxy", function
  * under the License.
  *
  */
- module.exports = {
-  registerApplication: function (success, fail) {//, params) {
-   //Params seem not needed on windows...
-   try {
-    var pushNotificationCallback = window.NotificationHub_onNotificationReceivedGlobal;
-    var pushNotificationHandler = function (e) {
-     var notificationTypeName = "";
-     var notificationPayload;
-     var msg = {};
+module.exports = {
+ registerApplication: function (success, fail) {//, params) {
+  //Params seem not needed on windows...
+  try {
+   var pushNotificationCallback = window.NotificationHub_onNotificationReceivedGlobal;
+   var pushNotificationHandler = function (e) {
+    var notificationTypeName = "";
+    var notificationPayload;
+    var msg = {};
 
-     try {
-      var notificationType = Windows.Networking.PushNotifications.PushNotificationType;
-      switch (e.notificationType) {
-       case notificationType.toast:
-        notificationPayload = e.toastNotification.content.getXml();
-        notificationTypeName = "Toast";
-        break;
-       case notificationType.tile:
-        notificationPayload = e.tileNotification.content.getXml();
-        notificationTypeName = "Tile";
-        break;
-       case notificationType.badge:
-        notification = e.badgeNotification.content.getXml();
-        notificationTypeName = "Badge";
-        break;
-       case notificationType.raw:
-        notificationPayload = e.rawNotification.content;
-        notificationTypeName = "Raw";
-        break;
-      }
-      msg.notificationTypeName = notificationTypeName;
-      msg.notificationPayload = notificationPayload;
-
-      pushNotificationCallback(msg);
-     } catch (ex) {
-      console.error("pushNotificationHandler::Exception caught", ex);
+    try {
+     var notificationType = Windows.Networking.PushNotifications.PushNotificationType;
+     switch (e.notificationType) {
+      case notificationType.toast:
+       notificationPayload = e.toastNotification.content.getXml();
+       notificationTypeName = "Toast";
+       break;
+      case notificationType.tile:
+       notificationPayload = e.tileNotification.content.getXml();
+       notificationTypeName = "Tile";
+       break;
+      case notificationType.badge:
+       notification = e.badgeNotification.content.getXml();
+       notificationTypeName = "Badge";
+       break;
+      case notificationType.raw:
+       notificationPayload = e.rawNotification.content;
+       notificationTypeName = "Raw";
+       break;
      }
-     e.cancel = true;
+     msg.notificationTypeName = notificationTypeName;
+     msg.notificationPayload = notificationPayload;
+
+     pushNotificationCallback(msg);
+    } catch (ex) {
+     console.error("pushNotificationHandler::Exception caught", ex);
+    }
+    e.cancel = true;
+   };
+
+   var notificationChannel = null;
+
+   Windows.Networking.PushNotifications.PushNotificationChannelManager.createPushNotificationChannelForApplicationAsync().then(function (channel) {
+    notificationChannel = channel;
+    notificationChannel.onpushnotificationreceived = pushNotificationHandler;
+    var msg = {
+     event: 'registerApplication',
+     registrationId: notificationChannel.uri
     };
+    success(msg);
+   }, fail);
 
-    var notificationChannel = null;
-
-    Windows.Networking.PushNotifications.PushNotificationChannelManager.createPushNotificationChannelForApplicationAsync().then(function (channel) {
-     notificationChannel = channel;
-     notificationChannel.onpushnotificationreceived = pushNotificationHandler;
-     var msg = {
-      event: 'registerApplication',
-      registrationId: notificationChannel.uri
-     };
-     success(msg);
-    }, fail);
-
-   } catch (ex) {
-    fail(ex);
-   }
-
-  },
-  unregisterApplication: function (success, fail, args) {
-   try {
-    //The following looks unsupported
-    //          (new NotificationHubRuntimeProxy.HubApi()).unregisterNativeAsync(notificationHubPath, connectionString);
-    success();
-
-   } catch (ex) {
-    fail(ex);
-   }
+  } catch (ex) {
+   fail(ex);
   }
 
- };
+ },
+ unregisterApplication: function (success, fail, args) {
+  try {
+   //The following looks unsupported
+   //          (new NotificationHubRuntimeProxy.HubApi()).unregisterNativeAsync(notificationHubPath, connectionString);
+   success();
 
- require("cordova/exec/proxy").add("NotificationHub", module.exports);
-});
+  } catch (ex) {
+   fail(ex);
+  }
+ }
+
+};
+
+require("cordova/exec/proxy").add("NotificationHub", module.exports);
